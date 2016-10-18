df08cb91577400899a0cf6c9d5d3fb6cef3c838c
duanli <dxl360@case.edu>
2016-10-18 19:51:38 -0400

    first

---
+++ app/src/main/java/com/example/duanli/student_portal/ItemListViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.duanli.student_portal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list_view);
+    }
+}
