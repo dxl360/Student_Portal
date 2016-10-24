4e8fec1f0e823b3862a8ea97c3a04f0ec9856d13
tianlin lu <txl355@case.edu>
2016-10-23 20:20:45 -0400

    listview

--- app/src/main/java/com/example/duanli/student_portal/ItemListViewActivity.java
+++ app/src/main/java/com/example/duanli/student_portal/ItemListViewActivity.java
@@ -1,13 +1,22 @@
 package com.example.duanli.student_portal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ItemListViewActivity extends AppCompatActivity {
+public class ItemListViewActivity extends Activity {
+    // Array of strings...
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list_view);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_item_list_view, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
     }
 }
