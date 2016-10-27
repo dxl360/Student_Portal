21d383dc0eeeb363172bbf1019986674bb971c65
tianlin lu <txl355@case.edu>
2016-10-27 10:14:13 -0400

    listview

--- app/src/main/java/com/example/duanli/student_portal/EventListViewActivity.java
+++ app/src/main/java/com/example/duanli/student_portal/EventListViewActivity.java
@@ -1,22 +1,52 @@
 package com.example.duanli.student_portal;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class EventListViewActivity extends Activity {
-    // Array of strings...
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+
+    public static final String TAG = EventListViewActivity.class.getSimpleName();
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_event_list_view);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_event_list_view, mobileArray);
+        final Context context = this;
 
-        ListView listView = (ListView) findViewById(R.id.mobile_list);
-        listView.setAdapter(adapter);
-    }
-}
+        // Get data to display
+        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
+
+        // Create adapter
+        ListViewAdapter adapter = new ListViewAdapter(this, recipeList);
+
+        // Create list view
+        mListView = (ListView) findViewById(R.id.recipe_list_view);
+        mListView.setAdapter(adapter);
+
+        // Set what happens when a list view item is clicked
+        mListView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Recipe selectedRecipe = recipeList.get(position);
+
+                Intent detailIntent = new Intent(context, EventDetailActivity.class);
+                detailIntent.putExtra("title", selectedRecipe.title);
+                detailIntent.putExtra("url", selectedRecipe.instructionUrl);
+
+                startActivity(detailIntent);
+            }
+
+        });
+    }}
