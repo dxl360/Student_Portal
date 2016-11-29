d56899734d1ea0523b79b886123c170a0e702c7f
zezephyrphyr <zephyr.jixy@gmail.com>
2016-11-28 20:58:01 -0500

    Relationship
    Updated Relationship with insert method in database helper.

--- app/src/main/java/com/example/duanli/student_portal/SPDatabaseHelper.java
+++ app/src/main/java/com/example/duanli/student_portal/SPDatabaseHelper.java
@@ -26,6 +26,60 @@ public class SPDatabaseHelper extends SQLiteOpenHelper{
     private static final String TABLE_ITEM = "item";
     private static final String TABLE_EVENT= "event";
 
+    private static final String RELATIONSHIP_SELL = "sell";
+    private static final String RELATIONSHIP_RESERVE="reserve";
+    private static final String RELATIONSHIP_WATCHLIST="watchlist";
+    private static final String RELATIONSHIP_BLOCK="block";
+    private static final String RELATIONSHIP_RATING="rating";
+    private static final String RELATIONSHIP_ORGANIZE="organize";
+    private static final String RELATIONSHIP_JOIN="join";
+    private static final String RELATIONSHIP_BOOKMARK="bookmark";
+    private static final String RELATIONSHIP_PERMIT="permit";
+
+    // User Relationship Columns
+    // Sell
+    private static final String KEY_SELL_ID = "itemId";
+    private static final String KEY_SELL_SELLER = "seller";
+    private static final String KEY_SELL_STATUS = "status";
+
+    // Reserve
+    private static final String KEY_RESERVE_ID = "reservationId";
+    private static final String KEY_RESERVE_ITEM = "itemId";
+    private static final String KEY_RESERVE_BUYER = "buyer";
+    private static final String KEY_RESERVE_SELLER = "seller";
+    private static final String KEY_RESERVE_STATUS = "status";
+
+    //Watchlist
+    private static final String KEY_WATCHLIST_ID = "watchlistId";
+    private static final String KEY_WATCHLIST_SELLER = "seller";
+    private static final String KEY_WATCHLIST_BUYER = "buyer";
+
+    //Block
+    private static final String KEY_BLOCK_SELLER = "seller";
+    private static final String KEY_BLOCK_BUYER = "buyer";
+
+    //Rating
+    private static final String KEY_RATING_RESERVATION = "reservationId";
+    private static final String KEY_RATING_SELLER = "seller";
+    private static final String KEY_RATING_BUYER = "buyer";
+    private static final String KEY_RATING_SCORE = "score";
+
+    //Organize
+    private static final String KEY_ORGANIZER_EVENT = "eventID";
+    private static final String KEY_ORGANIZER_ORGANIZER = "organizer";
+
+    //JOIN
+    private static final String KEY_JOIN_EVENT = "eventID";
+    private static final String KEY_JOIN_PARTICIPANT = "participant";
+
+    //BOOKMARK
+    private static final String KEY_BOOKMARK_EVENT = "eventID";
+    private static final String KEY_BOOKMARK_PARTICIPANT = "participant";
+
+    //Permit
+    private static final String KEY_PERMIT_EVENT = "eventID";
+    private static final String KEY_PERMIT_PARTICIPANT = "participant";
+
     // User Table Columns
     private static final String KEY_USER_ID = "id";
     private static final String KEY_USER_USERNAME= "username";
@@ -36,6 +90,9 @@ public class SPDatabaseHelper extends SQLiteOpenHelper{
     private static final String KEY_ITEM_NAME = "itemName";
     private static final String KEY_ITEM_Picture = "itemPictureUrl";
     private static final String KEY_ITEM_SELLER = "seller";
+
+    // EVENT Table Columns
+    private static final String KEY_EVENT_ID = "eventId";
 
     public static synchronized SPDatabaseHelper getInstance(Context context) {
         // Use the application context, which will ensure that you
@@ -76,8 +133,79 @@ public class SPDatabaseHelper extends SQLiteOpenHelper{
                 "FOREIGN KEY(" + KEY_ITEM_SELLER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_USERNAME + ")"+
         ")";
 
+        String CREATE_RESERVE_TABLE = "CREATE TABLE " + RELATIONSHIP_RESERVE +
+                "(" +
+                KEY_RESERVE_ID + " integer primary key autoincrement," +
+                KEY_RESERVE_STATUS + " INTEGER" + ","+
+                "FOREIGN KEY(" + KEY_RESERVE_SELLER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_RESERVE_BUYER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_RESERVE_ITEM + ") REFERENCES "+TABLE_ITEM+ "("+ KEY_ITEM_ID + ")"+
+                ")";
+
+        String CREATE_SELL_TABLE = "CREATE TABLE " + RELATIONSHIP_SELL +
+                "(" +
+                KEY_SELL_STATUS + " INTEGER" + ","+
+                "FOREIGN KEY(" + KEY_SELL_SELLER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_SELL_ID+ ") REFERENCES "+TABLE_ITEM+ "("+ KEY_ITEM_ID + ")"+
+                ")";
+
+        String CREATE_WATCHLIST_TABLE = "CREATE TABLE " + RELATIONSHIP_WATCHLIST +
+                "(" +
+                "FOREIGN KEY(" + KEY_WATCHLIST_BUYER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_WATCHLIST_SELLER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_WATCHLIST_ID+ ") REFERENCES "+TABLE_ITEM+ "("+ KEY_ITEM_ID + ")"+
+                ")";
+
+        String CREATE_BLOCK_TABLE = "CREATE TABLE " + RELATIONSHIP_BLOCK +
+                "(" +
+                "FOREIGN KEY(" + KEY_BLOCK_BUYER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_BLOCK_SELLER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+
+                ")";
+
+        String CREATE_RATING_TABLE = "CREATE TABLE " + RELATIONSHIP_RATING +
+                "(" +
+                KEY_RATING_SCORE + " DOUBLE" + ","+
+                "FOREIGN KEY(" + KEY_RATING_BUYER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_RATING_SELLER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_RATING_RESERVATION+ ") REFERENCES "+RELATIONSHIP_RESERVE+ "("+ KEY_RESERVE_ID + ")"+
+                ")";
+
+        String CREATE_ORGANIZE_TABLE = "CREATE TABLE " + RELATIONSHIP_ORGANIZE +
+                "(" +
+                "FOREIGN KEY(" + KEY_ORGANIZER_ORGANIZER + ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_ORGANIZER_EVENT+ ") REFERENCES "+TABLE_EVENT+ "("+ KEY_EVENT_ID + ")"+
+                ")";
+
+        String CREATE_JOIN_TABLE = "CREATE TABLE " + RELATIONSHIP_JOIN +
+                "(" +
+                "FOREIGN KEY(" + KEY_JOIN_PARTICIPANT+ ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_JOIN_EVENT+ ") REFERENCES "+TABLE_EVENT+ "("+ KEY_EVENT_ID + ")"+
+                ")";
+
+        String CREATE_BOOKMARK_TABLE = "CREATE TABLE " + RELATIONSHIP_BOOKMARK +
+                "(" +
+                "FOREIGN KEY(" + KEY_BOOKMARK_PARTICIPANT+ ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_BOOKMARK_EVENT+ ") REFERENCES "+TABLE_EVENT+ "("+ KEY_EVENT_ID + ")"+
+                ")";
+
+        String CREATE_PERMIT_TABLE = "CREATE TABLE " + RELATIONSHIP_PERMIT +
+                "(" +
+                "FOREIGN KEY(" + KEY_PERMIT_PARTICIPANT+ ") REFERENCES "+TABLE_USER+ "("+ KEY_USER_ID + ")"+","+
+                "FOREIGN KEY(" + KEY_PERMIT_EVENT+ ") REFERENCES "+TABLE_EVENT+ "("+ KEY_EVENT_ID + ")"+
+                ")";
+
         db.execSQL(CREATE_USER_TABLE);
         db.execSQL(CREATE_ITEM_TABLE);
+
+        db.execSQL(CREATE_RESERVE_TABLE );
+        db.execSQL(CREATE_SELL_TABLE);
+        db.execSQL(CREATE_WATCHLIST_TABLE);
+        db.execSQL(CREATE_BLOCK_TABLE );
+        db.execSQL(CREATE_RATING_TABLE);
+        db.execSQL(CREATE_ORGANIZE_TABLE);
+        db.execSQL(CREATE_JOIN_TABLE);
+        db.execSQL(CREATE_BOOKMARK_TABLE);
+        db.execSQL(CREATE_PERMIT_TABLE);
     }
 
     @Override
@@ -152,6 +280,209 @@ public class SPDatabaseHelper extends SQLiteOpenHelper{
         }
     }
 
+    // Insert a sell into the database
+    public boolean insertSell(int itemId, int seller, int status) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_SELL_ID, itemId);
+            values.put(KEY_SELL_SELLER, seller);
+            values.put(KEY_SELL_STATUS, status);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_SELL, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new sell to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a reservation into the database
+    public boolean insertReserve(int itemId, int seller, int buyer, int status) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_RESERVE_ITEM, itemId);
+            values.put(KEY_RESERVE_BUYER, buyer);
+            values.put(KEY_RESERVE_SELLER, seller);
+            values.put(KEY_RESERVE_STATUS, status);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_RESERVE, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new reservation to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a watchlist into the database
+    public boolean insertWatchlist(int itemId, int buyer, int seller) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_WATCHLIST_ID, itemId);
+            values.put(KEY_WATCHLIST_BUYER, buyer);
+            values.put(KEY_WATCHLIST_SELLER, seller);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_WATCHLIST, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new watchlist to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a block into the database
+    public boolean insertBlock(int buyer, int seller) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_BLOCK_BUYER, buyer);
+            values.put(KEY_BLOCK_SELLER, seller);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_BLOCK, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new block to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a rating into the database
+    public boolean insertRating(int reservationId, int buyer, int seller, double score) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_RATING_SCORE, score);
+            values.put(KEY_RATING_BUYER, buyer);
+            values.put(KEY_RATING_SELLER, seller);
+            values.put(KEY_RATING_RESERVATION, reservationId);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_RATING, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new rating to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a organize into the database
+    public boolean insertOrganize(int eventId, int organizer) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_ORGANIZER_EVENT, eventId);
+            values.put(KEY_ORGANIZER_ORGANIZER, organizer);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_ORGANIZE, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new organize to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a watchlist into the database
+    public boolean insertJoin(int eventId, int participant) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_JOIN_EVENT, eventId);
+            values.put(KEY_JOIN_PARTICIPANT, participant);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_JOIN, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new join to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a watchlist into the database
+    public boolean insertBookmark(int eventId, int participant) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_BOOKMARK_EVENT, eventId);
+            values.put(KEY_BOOKMARK_PARTICIPANT, participant);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_BOOKMARK, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new bookmark to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
+
+    // Insert a watchlist into the database
+    public boolean insertPermit(int eventId, int participant) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_PERMIT_EVENT, eventId);
+            values.put(KEY_PERMIT_PARTICIPANT, participant);
+            //check if this relationship already exists
+            //
+            //
+            //
+            db.insertOrThrow(RELATIONSHIP_PERMIT, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            System.out.println("Error while create new permit to database");
+        } finally {
+            db.endTransaction();
+        }
+        return true;
+    }
     /** Update the User Table, the method takes userI as input
      *  No Foreign key involved in the User Table
      *  The method use Username to locate the user
